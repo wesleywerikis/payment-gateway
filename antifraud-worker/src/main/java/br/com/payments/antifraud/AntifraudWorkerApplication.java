e7df712edfd8d00e4e1bfd73eb2eadecace49ea8
Wesley Werikis <wesleywerikis@outlook.com>
2025-11-05 16:50:16 -0300

    feat(antifraud-worker): add main application entry point

---
+++ antifraud-worker/src/main/java/br/com/payments/antifraud/AntifraudWorkerApplication.java
@@ -0,0 +1,13 @@
+package br.com.payments.antifraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntifraudWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntifraudWorkerApplication.class, args);
+	}
+
+}
