79f8d5eda42b0e80c188ad753dc5148842faf4eb
Wesley Werikis <wesleywerikis@outlook.com>
2025-11-05 16:30:25 -0300

    test(antifraud-worker): add default Spring Boot context load test

---
+++ antifraud-worker/src/test/java/br/com/payments/antifraud/AntifraudWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.payments.antifraud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntifraudWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
